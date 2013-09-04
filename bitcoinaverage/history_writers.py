e2a41bdbd396142f2ebe70ef184133f0150a36b9
alexykot <alexykot@gmail.com>
2013-09-04 17:43:23 +0100

    - history daemon reimplemented

--- bitcoinaverage/history_writers.py
+++ bitcoinaverage/history_writers.py
@@ -20,7 +20,7 @@ def write_default(currency_code):
             default_file.write(json.dumps(default_contents))
 
 
-def write_24h_csv(currency_code, current_data, last_timestamp):
+def write_24h_csv(currency_code, current_data, current_timestamp):
     current_24h_sliding_file_path = os.path.join(ba.server.HISTORY_DOCUMENT_ROOT, currency_code, '24h_sliding.csv')
 
     current_24h_sliding_data = []
@@ -34,8 +34,8 @@ def write_24h_csv(currency_code, current_data, last_timestamp):
             if not header_passed:
                 header_passed = True
                 continue
-            timestamp = time.mktime(datetime.datetime.strptime(row[0], '%Y-%m-%d %H:%M:%S').timetuple())
-            if last_timestamp - timestamp < 86400: #60*60*24
+            last_recorded_timestamp = time.mktime(datetime.datetime.strptime(row[0], '%Y-%m-%d %H:%M:%S').timetuple())
+            if current_timestamp - last_recorded_timestamp < 86400: #60*60*24
                 current_24h_sliding_data.append(row)
 
     last_recorded_timestamp = 0
@@ -43,9 +43,9 @@ def write_24h_csv(currency_code, current_data, last_timestamp):
         last_recorded_timestamp = time.mktime(datetime.datetime.strptime(current_24h_sliding_data[len(current_24h_sliding_data)-1][0],
                                                            '%Y-%m-%d %H:%M:%S').timetuple())
 
-    if last_timestamp - last_recorded_timestamp > 60:
+    if current_timestamp - last_recorded_timestamp > 60*2:
         #-60 added because otherwise the timestamp will point to the the beginning of next period and not current
-        current_24h_sliding_data.append([datetime.datetime.strftime(datetime.datetime.fromtimestamp(last_timestamp-60), '%Y-%m-%d %H:%M:%S'),
+        current_24h_sliding_data.append([datetime.datetime.strftime(datetime.datetime.fromtimestamp(current_timestamp-60), '%Y-%m-%d %H:%M:%S'),
                                          current_data['last']])
 
     with open(current_24h_sliding_file_path, 'wb') as csvfile:
@@ -55,7 +55,7 @@ def write_24h_csv(currency_code, current_data, last_timestamp):
             csvwriter.writerow(row)
 
 
-def write_1mon_csv(currency_code, last_timestamp):
+def write_1mon_csv(currency_code, current_timestamp):
     current_1h_1mon_sliding_file_path = os.path.join(ba.server.HISTORY_DOCUMENT_ROOT, currency_code, '1mon_sliding.csv')
 
     current_1mon_sliding_data = []
@@ -70,7 +70,7 @@ def write_1mon_csv(currency_code, last_timestamp):
                 header_passed = True
                 continue
             timestamp = time.mktime(datetime.datetime.strptime(row[0], '%Y-%m-%d %H:%M:%S').timetuple())
-            if last_timestamp - timestamp < 2592000: #60*60*24*30
+            if current_timestamp - timestamp < 2592000: #60*60*24*30
                 current_1mon_sliding_data.append(row)
 
     last_recorded_timestamp = 0
@@ -78,7 +78,7 @@ def write_1mon_csv(currency_code, last_timestamp):
         last_recorded_timestamp = time.mktime(datetime.datetime.strptime(current_1mon_sliding_data[len(current_1mon_sliding_data)-1][0],
                                                            '%Y-%m-%d %H:%M:%S').timetuple())
 
-    if int(time.time())-last_recorded_timestamp > 3600:
+    if int(time.time())-last_recorded_timestamp > 3600*2:
         current_24h_sliding_file_path = os.path.join(ba.server.HISTORY_DOCUMENT_ROOT, currency_code, '24h_sliding.csv')
         price_high = 0.0
         price_low = 0.0
@@ -92,7 +92,7 @@ def write_1mon_csv(currency_code, last_timestamp):
                     header_passed = True
                     continue
                 timestamp = time.mktime(datetime.datetime.strptime(row[0], '%Y-%m-%d %H:%M:%S').timetuple())
-                if last_timestamp - timestamp < 3600: #60*60*24
+                if current_timestamp - timestamp < 3600: #60*60*24
                     index = index + 1
                     price = float(row[1])
                     price_sum = price_sum + Decimal(price)
@@ -105,7 +105,7 @@ def write_1mon_csv(currency_code, last_timestamp):
             except(ZeroDivisionError):
                 price_avg = DEC_PLACES
         #-3600 added because otherwise the timestamp will point to the the beginning of next period and not current
-        current_1mon_sliding_data.append([datetime.datetime.strftime(datetime.datetime.fromtimestamp(last_timestamp-3600), '%Y-%m-%d %H:%M:%S'),
+        current_1mon_sliding_data.append([datetime.datetime.strftime(datetime.datetime.fromtimestamp(current_timestamp-3600), '%Y-%m-%d %H:%M:%S'),
                                           price_high,
                                           price_low,
                                           price_avg,
@@ -118,7 +118,7 @@ def write_1mon_csv(currency_code, last_timestamp):
                 csvwriter.writerow(row)
 
 
-def write_forever_csv(currency_code, total_sliding_volume, last_timestamp):
+def write_forever_csv(currency_code, total_sliding_volume, current_timestamp):
     current_forever_file_path = os.path.join(ba.server.HISTORY_DOCUMENT_ROOT, currency_code, 'forever.csv')
 
     if not os.path.exists(os.path.join(current_forever_file_path)) or os.path.getsize(current_forever_file_path) == 0:
@@ -136,7 +136,7 @@ def write_forever_csv(currency_code, total_sliding_volume, last_timestamp):
                 continue
             last_recorded_timestamp = time.mktime(datetime.datetime.strptime(row[0], '%Y-%m-%d %H:%M:%S').timetuple())
 
-    if last_timestamp - last_recorded_timestamp > 86400: #60*60*24
+    if current_timestamp - last_recorded_timestamp > 86400*2: #60*60*24
         current_24h_sliding_file_path = os.path.join(ba.server.HISTORY_DOCUMENT_ROOT, currency_code, '24h_sliding.csv')
         price_high = 0.0
         price_low = 0.0
@@ -150,7 +150,7 @@ def write_forever_csv(currency_code, total_sliding_volume, last_timestamp):
                     header_passed = True
                     continue
                 timestamp = time.mktime(datetime.datetime.strptime(row[0], '%Y-%m-%d %H:%M:%S').timetuple())
-                if last_timestamp - timestamp < 3600: #60*60*24
+                if current_timestamp - timestamp < 3600: #60*60*24
                     index = index + 1
                     price = float(row[1])
                     price_sum = price_sum + Decimal(price)
@@ -166,7 +166,7 @@ def write_forever_csv(currency_code, total_sliding_volume, last_timestamp):
         with open(current_forever_file_path, 'ab') as csvfile:
             csvwriter = csv.writer(csvfile, delimiter=',')
             #-86400 added because otherwise the timestamp will point to the the beginning of next period and not current
-            csvwriter.writerow([datetime.datetime.strftime(datetime.datetime.fromtimestamp(last_timestamp-86400), '%Y-%m-%d %H:%M:%S'),
+            csvwriter.writerow([datetime.datetime.strftime(datetime.datetime.fromtimestamp(current_timestamp-86400), '%Y-%m-%d %H:%M:%S'),
                                 price_high,
                                 price_low,
                                 price_avg,
@@ -174,9 +174,11 @@ def write_forever_csv(currency_code, total_sliding_volume, last_timestamp):
                                 ])
 
 
-def write_volumes_csv(currency_code, currency_data, last_timestamp):
+def write_volumes_csv(currency_code, currency_data, current_timestamp):
     current_volumes_file_path = os.path.join(ba.server.HISTORY_DOCUMENT_ROOT, currency_code, 'volumes.csv')
 
+    with open(current_volumes_file_path, 'a') as csvfile: #to create file if not exists
+        pass
 
     current_volumes_data = []
     exchanges_order = []
@@ -204,8 +206,7 @@ def write_volumes_csv(currency_code, currency_data, last_timestamp):
     if len(current_volumes_data) > 0:
         last_recorded_timestamp = time.mktime(datetime.datetime.strptime(current_volumes_data[len(current_volumes_data)-1][0],
                                                            '%Y-%m-%d %H:%M:%S').timetuple())
-
-    if last_timestamp - last_recorded_timestamp > 86400: #60*60*24
+    if current_timestamp - last_recorded_timestamp > 86400*2: #60*60*24; *2 because we check since start of yesterday till end of today
         for exchange in currency_data['exchanges']:
             if exchange not in exchanges_order:
                 exchanges_order.append(exchange)
@@ -213,143 +214,25 @@ def write_volumes_csv(currency_code, currency_data, last_timestamp):
                 headers.append('%s %%' % exchange)
 
         new_data_row = []
-        new_data_row.append(datetime.datetime.strftime(datetime.datetime.fromtimestamp(last_timestamp-86400), '%Y-%m-%d %H:%M:%S'))
-        new_data_row.append(currency_data[currency_code]['averages']['total_vol'])
+        new_data_row.append(datetime.datetime.strftime(datetime.datetime.fromtimestamp(current_timestamp-86400), '%Y-%m-%d %H:%M:%S'))
+        new_data_row.append(currency_data['averages']['total_vol'])
 
+        for exchange in exchanges_order:
+            if exchange in currency_data['exchanges']:
+                new_data_row.append(currency_data['exchanges'][exchange]['volume_btc'])
+                new_data_row.append(currency_data['exchanges'][exchange]['volume_percent'])
+            else:
+                new_data_row.append(0)
+                new_data_row.append(0)
+
+        new_data_row.append(currency_data['averages']['total_vol'])
 
         with open(current_volumes_file_path, 'wb') as csvfile:
             csvwriter = csv.writer(csvfile, delimiter=',')
-            csvwriter.writerow([datetime.datetime.strftime(datetime.datetime.fromtimestamp(last_timestamp), '%Y-%m-%d %H:%M:%S'),
-                                price_high,
-                                price_low,
-                                price_avg,
-                                total_sliding_volume,
-                                ])
+            csvwriter.writerow(headers)
+            for row in current_volumes_data:
+                csvwriter.writerow(row)
+
+            csvwriter.writerow(new_data_row)
 
 
-
-
-
-"""
- "USD": {
-    "averages": {
-      "ask": "137.38",
-      "bid": "136.75",
-      "last": "137.25",
-      "total_vol": "28708.04"
-    },
-    "exchanges": {
-      "bitbox": {
-        "rates": {
-          "ask": "134.99",
-          "bid": "131.29",
-          "last": "134.99"
-        },
-        "source": "bitcoincharts",
-        "volume_btc": "118.27",
-        "volume_percent": "0.41"
-      },
-      "bitkonan": {
-        "rates": {
-          "ask": "144.00",
-          "bid": "137.00",
-          "last": "140.00"
-        },
-        "source": "bitcoincharts",
-        "volume_btc": "8.50",
-        "volume_percent": "0.03"
-      },
-      "bitstamp": {
-        "rates": {
-          "ask": "129.93",
-          "bid": "129.91",
-          "last": "129.93"
-        },
-        "source": "api",
-        "volume_btc": "9640.58",
-        "volume_percent": "33.58"
-      },
-      "btce": {
-        "rates": {
-          "ask": "123.60",
-          "bid": "123.60",
-          "last": "123.60"
-        },
-        "source": "api",
-        "volume_btc": "2612.33",
-        "volume_percent": "9.10"
-      },
-      "campbx": {
-        "rates": {
-          "ask": "127.79",
-          "bid": "126.22",
-          "last": "127.79"
-        },
-        "source": "bitcoincharts",
-        "volume_btc": "617.10",
-        "volume_percent": "2.15"
-      },
-      "cryptotrade": {
-        "rates": {
-          "ask": "123.80",
-          "bid": "123.50",
-          "last": "123.50"
-        },
-        "source": "api",
-        "volume_btc": "8.58",
-        "volume_percent": "0.03"
-      },
-      "fbtc": {
-        "rates": {
-          "ask": "135.00",
-          "bid": "116.00",
-          "last": "125.00"
-        },
-        "source": "bitcoincharts",
-        "volume_btc": "22.00",
-        "volume_percent": "0.08"
-      },
-      "icbit": {
-        "rates": {
-          "ask": "150.00",
-          "bid": "85.00",
-          "last": "140.00"
-        },
-        "source": "bitcoincharts",
-        "volume_btc": "0.00",
-        "volume_percent": "0.00"
-      },
-      "localbitcoins": {
-        "rates": {
-          "ask": "146.33",
-          "bid": "None",
-          "last": "146.33"
-        },
-        "source": "api",
-        "volume_btc": "1285.30",
-        "volume_percent": "4.48"
-      },
-      "mtgox": {
-        "rates": {
-          "ask": "144.50",
-          "bid": "144.25",
-          "last": "144.25"
-        },
-        "source": "api",
-        "volume_btc": "14394.55",
-        "volume_percent": "50.14"
-      },
-      "vircurex": {
-        "rates": {
-          "ask": "132.73",
-          "bid": "132.00",
-          "last": "132.73"
-        },
-        "source": "api",
-        "volume_btc": "0.83",
-        "volume_percent": "0.00"
-      }
-    }
-  },
-
-  """
