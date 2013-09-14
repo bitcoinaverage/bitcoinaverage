2087a58138bfcbef9aab2900ac09a3d1eaf45713
bitnumus <bitnumus@gmail.com>
2013-09-14 22:31:08 +0100

    Working twitter bot

--- twitter_daemon.py
+++ twitter_daemon.py
@@ -2,9 +2,21 @@
 
 import twitter
 from bitcoinaverage.twitter_config import api
+import time
+import requests
 
 # requires  http://code.google.com/p/python-twitter/
 # https://github.com/bear/python-twitter.git
 
-status = api.PostUpdate('test')
-print status.text
+URL = "http://api.bitcoinaverage.com/ticker/USD"
+
+while True:
+    
+    r = requests.get(URL).json()
+    
+    status = "Average USD Rates - Last: %s / Ask: %s / Bid: %s / 24h: %s" % (r['last'],r['ask'],r['bid'],r['24h_avg'])
+    
+    status = api.PostUpdate(status)
+    print status.text
+    
+    time.sleep(60*60*4)
